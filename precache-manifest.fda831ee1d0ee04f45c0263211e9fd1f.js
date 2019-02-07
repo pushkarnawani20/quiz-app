97178af2ecb8a089f6bb07332939cd753391af31
pushkarnawani20 <pushkarnawani20@gmail.com>
2019-02-07 22:17:20 +0530

    Updates

---
+++ precache-manifest.fda831ee1d0ee04f45c0263211e9fd1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "026c744f4867be9b73b7",
+    "url": "/quiz-app/static/js/runtime~main.026c744f.js"
+  },
+  {
+    "revision": "3b50779e59edc8a10351",
+    "url": "/quiz-app/static/js/main.3b50779e.chunk.js"
+  },
+  {
+    "revision": "062e00ae8c0a3041c033",
+    "url": "/quiz-app/static/js/1.062e00ae.chunk.js"
+  },
+  {
+    "revision": "3b50779e59edc8a10351",
+    "url": "/quiz-app/static/css/main.e6f582d5.chunk.css"
+  },
+  {
+    "revision": "051ac09eb6941c20f8cb404ef45cd58b",
+    "url": "/quiz-app/index.html"
+  }
+];
